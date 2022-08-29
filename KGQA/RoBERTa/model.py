7adf20989de824db28e053cfdc870e64b4fb1d8e
RicardoL1u <2018212874@bupt.edu.cn>
2022-08-29 11:12:12 +0800

    fea: special token add

--- KGQA/RoBERTa/model.py
+++ KGQA/RoBERTa/model.py
@@ -11,7 +11,7 @@ import random
 
 class RelationExtractor(nn.Module):
 
-    def __init__(self, embedding_dim, relation_dim, num_entities, pretrained_embeddings, device, 
+    def __init__(self, embedding_dim, relation_dim, num_entities, pretrained_embeddings, device,tokenizer,special_tokens, 
     entdrop=0.0, reldrop=0.0, scoredrop=0.0, l3_reg=0.0, model='ComplEx', ls=0.0, do_batch_norm=True, freeze=True):
         super(RelationExtractor, self).__init__()
         self.device = device
