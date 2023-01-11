a95686f2ca8d0282cdb07d01f260f9bca4ba221e
RicardoL1u <2018212874@bupt.edu.cn>
2023-01-11 23:59:37 +0800

    ref: fit to iid ood

--- topic_entity_refine.py
+++ topic_entity_refine.py
@@ -12,7 +12,7 @@ def get_predicted_url(title:str)->str:
     # title = title.replace('\'','%27')
     return prefix+title
 
-data_path = '/data/lyt/exp/EmbedKGQA'
+data_path = '/data/lyt/exp/acl'
 topic_entity_set = set()
 
 with open("KGQA/RoBERTa/complex_wikidata5m.pkl", "rb") as fin:
@@ -35,14 +35,15 @@ with open('url2qid.json') as f:
 
 topic_entities = {
     'train':[],
-    'test':[],
-    'eval':[]
+    'valid':[],
+    'iid_test':[],
+    'ood_test':[],
 }
 
 regex = "EntityPage\/(Q[0-9]+)"
 
 
-for phase in ['test','eval','train']:
+for phase in ['iid_test','ood_test','valid','train']:
     datas = json.load(open(os.path.join(data_path,f'link_{phase}.json')))
     topic_entities[phase] = [None] * len(datas)
     for idx,data in tqdm(zip(range(len(datas)),datas)):
@@ -53,9 +54,12 @@ for phase in ['test','eval','train']:
             if entity_url in url2qid_map and url2qid_map[entity_url] in entity_5m_set:
                 qid = url2qid_map[entity_url]
                 topic_entity_set.add(qid)
-                topic_entities[phase][idx] = qid
+                if topic_entities[phase][idx] == None:
+                    topic_entities[phase][idx] = [qid]
+                else:
+                    topic_entities[phase][idx].append(qid)
                 mark = True
-                break
+                # break
         if not mark:
             print('Failed once')
             for tuple_str in data['pred_tuples_string']:
@@ -74,7 +78,7 @@ for phase in ['test','eval','train']:
                     url2qid_map[entity_url]=qids[0]
                     print('insert one!')
                     if topic_entities[phase][idx] == None and qids[0] in entity_5m_set:
-                        topic_entities[phase][idx] = qids[0]
+                        topic_entities[phase][idx] = [qids[0]]
                         topic_entity_set.add(qids[0])
                         print('find one!')
                 else:
@@ -83,11 +87,11 @@ for phase in ['test','eval','train']:
 with open('url2qid.json','w') as f:
     json.dump(url2qid_map,f,indent=4,ensure_ascii=False)
 
-assert all(topic_entities['train']) and all(topic_entities['eval']) and all(topic_entities['test'])
+assert all(topic_entities['train']) and all(topic_entities['valid']) and all(topic_entities['iid_test']) and all(topic_entities['ood_test'])
 assert topic_entity_set.issubset(entity_5m_set)
 
 with open(os.path.join(data_path,'link_result.json'),'w') as f:
     json.dump(topic_entities,f,indent=4,ensure_ascii=False)
 
-with open('topic_entity.json','w') as f:
-    json.dump(list(topic_entity_set),f,indent=4,ensure_ascii=False)
+# with open('topic_entity.json','w') as f:
+#     json.dump(list(topic_entity_set),f,indent=4,ensure_ascii=False)
