398ba2301dea409b8f40f8ee2003bd3f9ebb2c80
RicardoL1u <2018212874@bupt.edu.cn>
2023-01-20 11:18:14 +0800

    fea: metric result script

--- KGQA/RoBERTa/metric_result.py
+++ KGQA/RoBERTa/metric_result.py
@@ -1,32 +1,38 @@
 import json
 from single import Metric
-datapath = '/data/lyt/exp/rush/embedKGQA/human/'
-import pickle
-kg_name = 'complex_wyf_big5m'
-wiki5m = pickle.load(open(kg_name+'.pkl','rb'))
-entity2idx = wiki5m.graph.entity2id
+datapath = 'checkpoints/roberta_finetune/'
+# import pickle
+kg_name = 'wyf-500-wikidata5m'
+# wiki5m = pickle.load(open(kg_name+'.pkl','rb'))
+# entity2idx = wiki5m.graph.entity2id
 
-print('loading wikidata qid2entity_name')
-with open('/data/lyt/wikidata-5m/wikidata-5m-entity-en-label.json') as f:
-    qid2entity_name_map = json.load(f)
-print('loaded qid 2 entity name')
+# print('loading wikidata qid2entity_name')
+# with open('/data0/lyt/wikidata-5m/wikidata-5m-entity-en-label.json') as f:
+#     qid2entity_name_map = json.load(f)
+# print('loaded qid 2 entity name')
 
-for file in [f'{kg_name}_small_iid_test.json',f'{kg_name}_small_ood_test.json',f'{kg_name}_valid.json']:
-    dataset = json.load(open(datapath+file))
-    total_pred_list = []
-    for data in dataset:
-        if 'pred_names' not in data.keys():
-            data['pred_names'] = [qid2entity_name_map[qid] for qid in data['pred_qids']]
-        total_pred_list.append(list(set([pred['label'] for pred in data['pred_names']])))
-    assert len(data['answers']) == len(set(data['ans_ids']))
-    total_ans_list = [list(set(data['answers'])) for data in dataset]
-    if 'pred_names' not in data.keys():
-        with open(datapath+file,'w') as f:
-            json.dump(dataset,f,indent=4,ensure_ascii=False)
-    print(Metric.str_metric(total_pred_list,total_ans_list))
+for question_type in ['human','gpt','template']:
+    print("="*75 + question_type + "="*75)
+    for result_type in ['human','gpt','template']:
+        print("*"*50 + result_type + "="*50)
+        for file in [f'{kg_name}_{result_type}_small_iid_test.json',f'{kg_name}_{result_type}_small_ood_test.json']:
+            dataset = json.load(open(datapath+question_type+"/"+file))
+            total_pred_name_list = [[pred['label'] for pred in data['pred_names']] for data in dataset]        
+            total_ans_name_list = [data['answers'] for data in dataset]
+
+            total_pred_id_list = [data['pred_qids'] for data in dataset]        
+            total_ans_id_list = [data['ans_ids'] for data in dataset]
+
+            em_acc = Metric.str_metric(total_pred_name_list,total_ans_name_list)['em_acc_with_penalty']
+            token_f1 = Metric.str_metric(total_pred_id_list,total_ans_id_list)['token_f1_with_penalty']
+
+            print({
+                'em_acc':em_acc,
+                'token_f1':token_f1,
+            })
 
 # for file in ['train','valid','iid_test','ood_test']:
-#     dataset = json.load(open('/data/lyt/exp/acl/embedKGQA/'+file+'.json'))
+#     dataset = json.load(open('/data0/lyt/exp/acl/embedKGQA/'+file+'.json'))
 #     for data in dataset:
 #         for ans in data['ans_ids']:
 #             if ans not in entity2idx.keys():
